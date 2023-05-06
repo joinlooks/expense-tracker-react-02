f27607bbf55f331483a64c9491faa2a6ee9747b4
Lakshya Sharma <joinlooks@gmail.com>
2023-05-06 08:17:44 +0530

    debug: error found - path was incorrect in dist/index.html

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import { GlobalProvider } from "./contexts/GlobalState";
 function App() {
 	return (
 		<div className="flex justify-center flex-col max-w-sm mx-auto">
-			<h1>DEbug</h1>
 			<GlobalProvider>
 				<Header />
 				<div className="px-5">
